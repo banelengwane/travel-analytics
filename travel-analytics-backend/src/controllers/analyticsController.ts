c72434fd8afff86c5f130ee02528234da2017fab
banelengwane <banelengwane@gmail.com>
2026-06-10 15:44:46 +0200

    added monthly data aggregation

--- travel-analytics-backend/src/controllers/analyticsController.ts
+++ travel-analytics-backend/src/controllers/analyticsController.ts
@@ -31,3 +31,39 @@ export const getTravelStats = async (req: Request, res: Response): Promise<void>
         res.status(500).json({ message: 'Error computing backend analytics pipelines', error})
     }
 };
+
+export const getMonthlyExpenses = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const userId = req.user.id;
+
+        const monthlyData = await Trip.aggregate([
+            { $match: { user: new Types.ObjectId(userId) } },
+            { $unwind: '$expenses' },
+            {
+                $group: {
+                    _id: {
+                        month: { $month: '$startDate' },
+                        year: { $year: 'startDate'}
+                    },
+                    totalAmount: { $sum: '$expenses.amount' }
+                }
+            },
+            { $sort: { '_id.year': 1, '_id.month': 1 } },
+            {
+                $project: {
+                    date: {
+                        $concat: [
+                            { $toString: '$_id.year' }, "-", { $toString: '$_id.month' }
+                        ]
+                    },
+                    amount: '$totalAmount',
+                    _id: 0
+                }
+            }
+        ]);
+
+        res.status(200).json(monthlyData);
+    } catch (error) {
+        res.status(500).json({ message: "Failed to generate monthly aggregation data pipeline", error});
+    }
+};
